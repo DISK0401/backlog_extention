c4965b189a1ef22ff07a7a92554458c3969dbe70
Daisuke Tomioka <mail@disk0401.net>
2023-04-30 21:25:53 +0900

    [feat] plain付きクリップボードコピーに対応

--- js/content.js
+++ js/content.js
@@ -13,6 +13,7 @@ function main(event) {
 
             // 課題キーコピーのヘルプエリアを取得
             let copyKeyHelpArea = document.querySelector("#copyKey-help");
+            htmlAndPlain(copyKeyHelpArea);
             onlyHTML(copyKeyHelpArea);
         }
     }
@@ -20,7 +21,7 @@ function main(event) {
 
 /**
  * チケットのキーとタイトルをコピーするボタンを追加する(text/htmlのみ)
- * @param {*} existingButton 
+ * @param {*} existingButton ボタン表示エリア
  */
 function onlyHTML(existingButton) {
     // ボタン要素を作成
@@ -43,5 +44,35 @@ function onlyHTML(existingButton) {
         let data = [new ClipboardItem({ [blob.type]: blob })];
 
         copyToClipboard(data);
+    });
+};
+
+/**
+ * チケットのキーとタイトルをコピーするボタンを追加する(text/html, text/plain)
+ * @param {*} existingButton ボタン表示エリア
+ */
+function htmlAndPlain(existingButton) {
+    // ボタン要素を作成
+    let newButton = document.createElement('button');
+    newButton.classList.add('icon-button', 'icon-button--default','simptip-position-right', 'simptip-movable', 'simptip-smooth','simptip-multiline', '-copy-button-multiline');
+    newButton.insertAdjacentHTML('afterbegin','<span class="copy-trigger"><svg role="image" class="icon -medium"><use xlink:href="/images/svg/sprite.symbol.svg#icon_copy"></use></svg></span>');
+    newButton.dataset.tooltip = "基本的なリンク付きコピー";
+
+    // ボタンを追加
+    existingButton.appendChild(newButton);
+
+    // ボタンが押された時の処理
+    newButton.addEventListener('click', function() {
+        let currentUrl = window.location.href;
+        let ticket_id = document.querySelector(".ticket__key-number").innerText;
+        let subject = document.querySelector(".markdown-body").innerText;
+
+        let htmlLink = '<a href="' + currentUrl + '">' + ticket_id + '</a> ' + subject;
+        const blob = new Blob([htmlLink], { type:'text/html' });
+        const blobPlain = new Blob([htmlLink],{type:'text/plain'})
+        const data = [new ClipboardItem({ [blob.type]: blob, [blobPlain.type]: blobPlain })];
+
+        copyToClipboard(data);
+        copySuccess();
     });
 };
